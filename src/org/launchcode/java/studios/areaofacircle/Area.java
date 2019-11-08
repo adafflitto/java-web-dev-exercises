086b08631da79e08f58438dfa8dcea48399e2d1b
adafflitto <amdyt7@mail.umsl.edu>
2019-11-07 19:44:17 -0600

    print the area of a circle while validating and using method from another class

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,23 @@
+package org.launchcode.java.studios.areaofacircle;
+
+
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String [] args) {
+        Scanner input;
+        input = new Scanner(System.in);
+        double radius;
+        do {
+            System.out.println("What is the radius of your circle?");
+            while (!input.hasNextDouble()) {
+                System.out.println("Please enter a valid number!");
+                input.next();
+            }
+            radius = input.nextDouble();
+        } while (radius <= 0);
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+    }
+}
